c04e31356112e20a46f677f7af8fe4f1afe67c02
retina <xcy0115@163.com>
2019-11-14 21:41:44 +0800

    word

---
+++ Shopping/src/main/java/com/javaweb/mapper/DepMapper.java
@@ -0,0 +1,4 @@
+package com.javaweb.mapper;
+
+public interface DepMapper {
+}
--- Shopping/src/main/java/com/javaweb/service/Impl/DepServiceImpl.java
+++ Shopping/src/main/java/com/javaweb/service/Impl/DepServiceImpl.java
@@ -2,16 +2,21 @@ package com.javaweb.service.Impl;
 
 import com.javaweb.model.Dep;
 import com.javaweb.service.DepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DepServiceImpl implements DepService {
-
-
+    @Autowired
+    DepMapper depMapper;
     @Override
-    public List<Dep> searchByDepId(String depId) throws Exception {
-        return null;
+    public List<Dep> searchByDepId(String depId)throws Exception{
+        // TODO Auto-generated method stub
+        System.out.println("searchByDepId:"+depId);
+        List<Dep> dep = depMapper.searchByDepId(depId);
+        return dep;
     }
+
 }
