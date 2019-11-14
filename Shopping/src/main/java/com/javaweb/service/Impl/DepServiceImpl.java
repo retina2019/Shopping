67a03fd408225b3880ced1cab60529eab0e6f2da
retina <xcy0115@163.com>
2019-11-14 20:33:21 +0800

    xiugai

--- Shopping/src/main/java/com/javaweb/service/Impl/DepServiceImpl.java
+++ Shopping/src/main/java/com/javaweb/service/Impl/DepServiceImpl.java
@@ -1,10 +1,17 @@
 package com.javaweb.service.Impl;
 
+import com.javaweb.model.Dep;
 import com.javaweb.service.DepService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepServiceImpl implements DepService {
 
 
+    @Override
+    public List<Dep> searchByDepId(String depId) throws Exception {
+        return null;
+    }
 }
